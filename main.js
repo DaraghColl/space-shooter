cd2b2ff2de62a75eaa6c3fd90aca4cc118135448
daraghcoll <daraghcoll-@hotmail.com>
2022-04-13 01:19:10 +0100

    feat: create game loop, refactor shot code and key state handling

--- main.js
+++ main.js
@@ -2,37 +2,70 @@ import './style.css';
 
 const player = document.getElementById('player');
 const playerRect = document.getElementById('player-rect');
-const enemy = document.getElementById('enemy');
-const enemy2 = document.getElementById('enemy2');
-const test = document.getElementById('test');
 const allEnemies = document.querySelectorAll('.enemy');
+let keyState;
+let isShooting = false;
+let shootingInterval;
 
-player.style.left = `10px`;
+const initGame = () => {
+  player.style.left = `10px`;
 
-window.addEventListener('keydown', (e) => {
+  gameLoop();
+};
+
+const gameLoop = () => {
+  update();
+  window.requestAnimationFrame(gameLoop);
+};
+
+// update to be called repeatedely from game loop
+const update = () => {
+  // handle player position based on key state
+  let position = parseInt(player.style.left.slice(0, -2));
+
+  if (keyState === 'a') {
+    position -= 5;
+    player.style.left = position;
+  } else if (keyState === 'd') {
+    position += 5;
+    player.style.left = `${position}px`;
+  }
+};
+
+/** key up and down handler **/
+// key down handler
+document.addEventListener('keydown', (e) => {
   if (e.key === 'a') {
-    const currentPosition = parseInt(player.style.left.slice(0, -2));
-    const newPosition = currentPosition - 10;
-    player.style.left = `${newPosition}px`;
+    keyState = 'a';
   } else if (e.key === 'd') {
-    const currentPosition = parseInt(player.style.left.slice(0, -2));
-    const newPosition = currentPosition + 10;
-    player.style.left = `${newPosition}px`;
+    keyState = 'd';
   }
 
   if (e.key === ' ') {
-    const positionY = playerRect.getBoundingClientRect().top - 20;
-    const positionX =
-      playerRect.getBoundingClientRect().x +
-      playerRect.getBoundingClientRect().width -
-      35;
-
-    const bullet = createBullet(positionX, positionY);
-
-    document.getElementById('app').insertAdjacentElement('beforeend', bullet);
+    if (!isShooting) {
+      shootBullet();
+      shootingInterval = setInterval(shootBullet, 200);
+      isShooting = true;
+    }
   }
 });
 
+// key up handler
+document.addEventListener('keyup', (e) => {
+  if (e.key === 'a') {
+    keyState = '';
+  } else if (e.key === 'd') {
+    keyState = '';
+  }
+
+  if (e.key === ' ') {
+    isShooting = false;
+    clearInterval(shootingInterval);
+  }
+});
+
+/** bullet functionality **/
+// create bullet
 const createBullet = (x, y) => {
   const bullet = document.createElement('div');
   bullet.classList.add('bullet-div');
@@ -56,19 +89,49 @@ const createBullet = (x, y) => {
   return bullet;
 };
 
-function checkCollision(bullet) {
+// shoot bullet
+const shootBullet = () => {
+  const positionY = playerRect.getBoundingClientRect().top - 20;
+  const positionX =
+    playerRect.getBoundingClientRect().x +
+    playerRect.getBoundingClientRect().width -
+    35;
+
+  const bullet = createBullet(positionX, positionY);
+
+  document.getElementById('app').insertAdjacentElement('beforeend', bullet);
+};
+
+/** collision detection **/
+const checkCollision = (bullet) => {
   const bulletPositon = bullet.getBoundingClientRect();
 
   allEnemies.forEach((enemy) => {
     const enemyPositon = enemy.getBoundingClientRect();
 
     if (
-      bulletPositon.y < enemyPositon.bottom &&
-      bulletPositon.y > enemyPositon.top &&
-      bulletPositon.x > enemyPositon.left - 5 &&
-      bulletPositon.x < enemyPositon.right - 5
+      elementsCollide(
+        bulletPositon.x,
+        bulletPositon.y,
+        bulletPositon.width,
+        bulletPositon.height,
+        enemyPositon.x,
+        enemyPositon.y,
+        enemyPositon.width,
+        enemyPositon.height
+      )
     ) {
       enemy.remove();
     }
   });
-}
+};
+
+// check elements overlap
+const elementsCollide = (x1, y1, w1, h1, x2, y2, w2, h2) => {
+  if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2) {
+    return false;
+  }
+  return true;
+};
+
+initGame();
